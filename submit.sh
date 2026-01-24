f500782be81e5b1d78100d4244b15048c8bf312d
Hutton Grabiel <huttongrabiel@gmail.com>
2026-01-24 12:46:42 -0800

    submit.sh: update names

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "Hutton Grabiel,hgrabiel\nJohnathan Chi,johnchi\nRohan Simon Shanthanu,rshanth" > submit/team.txt
 
 # train model
 python src/myprogram.py train --work_dir work
