73d75f3184ea1973160c4808ed5e6807e39948dd
Hutton Grabiel <huttongrabiel@gmail.com>
2026-02-12 21:18:08 -0800

    fix indentation

--- src/ngram.py
+++ src/ngram.py
@@ -8,23 +8,23 @@ class UnigramModel:
     self.text = text
     self.probs = {}
 
-    # Build unigram probability distribution from training text
-    def fit(self):
-      for char in self.text:
-        if char not in self.probs:
-            self.probs[char] = 0
-        self.probs[char] += 1
-      
-      for char in self.probs:
-        self.probs[char] /= len(self.text)
+   # Build unigram probability distribution from training text
+  def fit(self):
+    for char in self.text:
+      if char not in self.probs:
+          self.probs[char] = 0
+      self.probs[char] += 1
     
-    def predict(self, prefixes):
-      # prefixes is a list of strings, each string is a prefix to predict the next character for
-      # returns a list of predicted next characters, one for each prefix
-      # Select the top three most probable characters to finish to prefix
-      preds = []
-      for _ in prefixes:
-        sorted_chars = sorted(self.probs, key=self.probs.get, reverse=True)
-        top_chars = sorted_chars[:3]
-        preds.append(''.join(top_chars))
-      return preds
+    for char in self.probs:
+      self.probs[char] /= len(self.text)
+  
+  def predict(self, prefixes):
+    # prefixes is a list of strings, each string is a prefix to predict the next character for
+    # returns a list of predicted next characters, one for each prefix
+    # Select the top three most probable characters to finish to prefix
+    preds = []
+    for _ in prefixes:
+      sorted_chars = sorted(self.probs, key=self.probs.get, reverse=True)
+      top_chars = sorted_chars[:3]
+      preds.append(''.join(top_chars))
+    return preds
